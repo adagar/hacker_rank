7faa11b52473ce571d45934d6e1822a13b403ec7
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2019-04-05 06:17:27 -0400

    New solution to this old algorithm

--- algorithms/sock_merchant.js
+++ algorithms/sock_merchant.js
@@ -1,30 +1,32 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
+const fs = require("fs");
 
 process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.setEncoding("utf-8");
 
-let inputString = '';
+let inputString = "";
 let currentLine = 0;
 
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
+process.stdin.on("data", inputStdin => {
+  inputString += inputStdin;
 });
 
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
+process.stdin.on("end", _ => {
+  inputString = inputString
+    .replace(/\s*$/, "")
+    .split("\n")
+    .map(str => str.replace(/\s*$/, ""));
 
-    main();
+  main();
 });
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++];
 }
 
 // Complete the sockMerchant function below.
+/*
 function sockMerchant(n, ar) {
     let counter = 0;
     for(let i = 1; i < ar.length; i++)
@@ -40,19 +42,38 @@ function sockMerchant(n, ar) {
                 }
         }
         return counter;
+}
+*/
 
+// Complete the sockMerchant function below.
+function sockMerchant(n, ar) {
+  let pairs = 0;
+  while (ar.length > 0) {
+    let seekingSock = ar.shift();
+    for (let i = 0; i < ar.length; i++) {
+      if (seekingSock === ar[i]) {
+        ar.splice(i, 1);
+        pairs++;
+        //need a break here or else it clears out EVERY matching fella for this sock
+        break;
+      }
+    }
+  }
+  return pairs;
 }
 
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
 
-    const n = parseInt(readLine(), 10);
+  const n = parseInt(readLine(), 10);
 
-    const ar = readLine().split(' ').map(arTemp => parseInt(arTemp, 10));
+  const ar = readLine()
+    .split(" ")
+    .map(arTemp => parseInt(arTemp, 10));
 
-    let result = sockMerchant(n, ar);
+  let result = sockMerchant(n, ar);
 
-    ws.write(result + "\n");
+  ws.write(result + "\n");
 
-    ws.end();
+  ws.end();
 }
