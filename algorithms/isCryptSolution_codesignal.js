14a0b32dada9e6e3791da045e365c2be1d3eeeb8
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2019-05-09 10:44:17 -0500

    Completed algorithm isCryptSolution

--- algorithms/isCryptSolution_codesignal.js
+++ algorithms/isCryptSolution_codesignal.js
@@ -1,21 +1,15 @@
 function isCryptSolution(crypt, solution) {
   //I'd prefer to work with an obj so a quick function to convert
   const solutionObject = convertArrayToObject(solution);
-  //console.log(solutionObject);
   let expectedSum = 0;
   let actualSum = 0;
 
   const cryptIntVal_0 = convertWordToValue(crypt[0], solutionObject);
-  console.log("Crypt int value 1: ", cryptIntVal_0);
   const cryptIntVal_1 = convertWordToValue(crypt[1], solutionObject);
-  console.log(cryptIntVal_1);
   const cryptIntVal_2 = convertWordToValue(crypt[2], solutionObject);
-  console.log(cryptIntVal_2);
-
+  console.log([cryptIntVal_0, cryptIntVal_1, cryptIntVal_2]);
   if (
-    cryptIntVal_0[0] === "0" ||
-    cryptIntVal_1[0] === "0" ||
-    cryptIntVal_2[0] === "0" ||
+    !validEquation([cryptIntVal_0, cryptIntVal_1, cryptIntVal_2]) ||
     parseInt(cryptIntVal_0) + parseInt(cryptIntVal_1) !==
       parseInt(cryptIntVal_2)
   ) {
@@ -24,6 +18,14 @@ function isCryptSolution(crypt, solution) {
   return true;
 }
 
+const validEquation = cryptIntegers => {
+  for (let i = 0; i < cryptIntegers.length; i++) {
+    if (cryptIntegers[i][0] === "0" && cryptIntegers[i].length > 1) {
+      return false;
+    }
+  }
+  return true;
+};
 const convertArrayToObject = arrayContent => {
   let newObj = {};
   for (let i = 0; i < arrayContent.length; i++) {
@@ -33,12 +35,10 @@ const convertArrayToObject = arrayContent => {
 };
 
 const convertWordToValue = (word, key) => {
-  console.log(word);
   const wordArr = word.split("");
-  console.log(wordArr);
   let numList = "";
-  for (let i = 0; i < wordArr; i++) {
-    numList += key.wordArr[i];
+  for (let i = 0; i < wordArr.length; i++) {
+    numList += key[wordArr[i]];
   }
   return numList;
 };
