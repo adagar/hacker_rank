54723970c81170e8f1162a369691f9816aab549b
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2019-06-05 15:38:03 -0400

    New algorithm

---
+++ algorithms/allLongestString.js
@@ -0,0 +1,11 @@
+function allLongestStrings(inputArray) {
+  let longest = 0;
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i].length > longest) {
+      longest = inputArray[i].length;
+    }
+  }
+  return inputArray.filter(word => {
+    return word.length === longest;
+  });
+}
